c1485fa28d0a44f324830cc17e7d7d00e940b1e9
hemanth krishna <hemanthsankara@gmail.com>
2023-11-08 16:14:50 +0530

    Personal blog project setup

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+declare var apiConfig: any;
+export const environment = {
+  production: true,
+  learningApiBaseUrl: 'http://localhost:1010/personalBlog',
+  uiTemplateBaseUrl: document.getElementsByTagName('base')[0].href + '/resources/static/src/assets',
+};
