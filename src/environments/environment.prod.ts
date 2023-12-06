6b3a7065a4e6379def50293b9a1fb5ef39560c19
hemanth krishna <hemanthsankara@gmail.com>
2023-12-06 11:41:54 +0530

    push docs folder

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,4 +4,5 @@ export const environment = {
   title: 'personalBlog-Production',
   href: document.getElementsByTagName('base')[0].href,
   apiBaseUrl: 'assets/jsons/mock-api/',
+  assetsPath: document.getElementsByTagName('base')[0].href + 'assets'
 };
