29c0f28e707cb239dd3717d65f84010524a341c9
hemanth krishna <hemanthsankara@gmail.com>
2023-12-13 17:41:40 +0530

    achievements changes

--- src/app/modules/achievements/achievements.component.ts
+++ src/app/modules/achievements/achievements.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { ApiConstants } from 'src/app/core/constants/api.constants';
 import { Achievement } from './model/achievement';
 import { AchievementsService } from './achievements.service';
@@ -12,6 +12,9 @@ export class AchievementsComponent {
 
   achievementsApi: string = ApiConstants.FETCH_ACHIVEMENTS_API;
   achievements: Achievement[] | undefined;
+  openImgPopUp:boolean = false;
+  displayStyle:string = 'none';
+  popUpImgUrl:string = '';
 
   constructor(private achievementsService: AchievementsService) {}
 
@@ -26,5 +29,13 @@ export class AchievementsComponent {
       complete: () => console.info("complete")
     })
   }
+  
+  openImgPopup(imgUrl:string) { 
+    this.popUpImgUrl = imgUrl;
+    this.displayStyle = "block"; 
+  } 
+  closeImgPopup() { 
+    this.displayStyle = "none"; 
+  } 
 
 }
